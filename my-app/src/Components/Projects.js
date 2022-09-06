ab8da1b142cd3e02c8c746fbf9a622c256ac8664
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-09-06 23:04:50 +0200

    projects

--- my-app/src/Components/Projects.js
+++ my-app/src/Components/Projects.js
@@ -6,10 +6,14 @@ const Projects = () => {
             <section className="projects">
                 <div className="projects__description">
                     <h1>Projects</h1>
-                    <div className="projects__image"><a href="https://harrypotter-quiz.netlify.app/">
-                        Harry Potter Quiz</a></div>
+                    <div className="separator"></div>
                     <p>lorem........</p>
                 </div>
+                <a target="blank" href="https://harrypotter-quiz.netlify.app/">
+                    <div className="projects__image"></div>
+                    <h2>Harry Potter Quiz</h2>
+                </a>
+                
             </section>        
         </>
     )
