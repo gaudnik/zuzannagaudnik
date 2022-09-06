e70895a43d6f33e09815c8848c266ab3590fa992
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-09-06 15:38:47 +0200

    add about me section

--- my-app/src/Components/AboutMe.js
+++ my-app/src/Components/AboutMe.js
@@ -2,15 +2,18 @@ import React from "react";
 
 const AboutMe = () => {
     return (
-        <section name="aboutme" className="about__me">
-            <div className="about__me__description">
-                <h1>O mnie</h1>
+        <section name="aboutme" className="aboutme">
+            <div className="aboutme__image"></div>
+
+            <div className="aboutme__description">
+                <h1>About me</h1>
                 <div className="separator"></div>
-                <p className="about__me__text">I am a front-end developer who is looking 
+                <p className="aboutme__text">I am a front-end developer who is looking 
                 for new challenges and opportunities to develop programming skills. 
                 During the last year of working as a freelancer, I realized that I need stabilization on my professional path.</p>
+                <a  target="blank" className="btn" href="https://drive.google.com/file/d/1NcrcO5DTHo8GC_40yum4ZsChvhrIqFER/view?usp=sharing" >
+                    Download my CV</a>
             </div>
-            <div className="about__me__image"></div>
         </section>
     )
 }
