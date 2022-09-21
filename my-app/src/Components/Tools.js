9e05134fa790ff988d03b191d77f3b6cf8416bda
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-09-21 17:47:20 +0200

    add tools

--- my-app/src/Components/Tools.js
+++ my-app/src/Components/Tools.js
@@ -7,22 +7,22 @@ const Tools = () => {
                 <h1 className="tools__header">My tools</h1>
                 <div className="separator"></div>
                 <div className="tools__box">
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
-                    <div className="tools__tool"></div>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="blank"></a>
+                    <a className="tools__tool" href="https://reactjs.org/" target="blank"></a>
+                    <a className="tools__tool" href="https://nodejs.org/en/docs/" target="blank"></a>
+                    <a className="tools__tool" href="https://babeljs.io/docs/en/" target="blank"></a>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="blank"></a>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank"></a>
+                    <a className="tools__tool" href= "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
+                    <a className="tools__tool" href= "" target="blank"></a>
                 </div>
 
             </section>
