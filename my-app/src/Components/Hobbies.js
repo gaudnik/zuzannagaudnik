e324b33188363256decf436048fe11a86c485a14
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-09-23 19:11:13 +0200

    hobbies

--- my-app/src/Components/Hobbies.js
+++ my-app/src/Components/Hobbies.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hobbies = () => {
-
+    return (
+        <section className="hobbies">
+            <h1 className="hobbies__header">My Hobbies</h1>
+            <div className="separator"></div>
+            <p className="hobbies__text">lorem...</p>
+            <div className="hobbies__box">
+                <a className="hobbies__hobby" href="" target="blank"></a>
+                <a className="hobbies__hobby" href="" target="blank"></a>
+                <a className="hobbies__hobby" href="" target="blank"></a>
+                <a className="hobbies__hobby" href="" target="blank"></a>
+            </div>
+        </section>
+    )
 }
 
 export default Hobbies;
