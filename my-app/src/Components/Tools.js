7570c6589ce546acbecf6f4316778a051f48bed5
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-01-14 12:03:14 +0100

    tools

--- my-app/src/Components/Tools.js
+++ my-app/src/Components/Tools.js
@@ -11,24 +11,22 @@ const Tools = () => {
                     attention because it uses variables, nested rules, mixins, functions and many other solutions.
                     I am currently learning Node.js and TypeScript.</p>
                 <div className="tools__box">
-                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="blank"></a>
-                    <a className="tools__tool" href="https://reactjs.org/" target="blank"></a>
-                    <a className="tools__tool" href="https://nodejs.org/en/docs/" target="blank"></a>
-                    <a className="tools__tool" href="https://babeljs.io/docs/en/" target="blank"></a>
-                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="blank"></a>
-                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank"></a>
-                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON" target="blank"></a>
-                    <a className="tools__tool" href="https://gulpjs.com/" target="blank"></a>
-                    <a className="tools__tool" href="https://webpack.js.org/" target="blank"></a>
-                    <a className="tools__tool" href="https://sass-lang.com/documentation/" target="blank"></a>
-                    <a className="tools__tool" href="https://docs.npmjs.com/" target="blank"></a>
-                    <a className="tools__tool" href="https://firebase.google.com/docs" target="blank"></a>
-                    <a className="tools__tool" href="https://www.atlassian.com/software/jira" target="blank"></a>
-                    <a className="tools__tool" href="https://github.com/" target="blank"></a>
-                    <a className="tools__tool" href="https://git-scm.com/doc" target="blank"></a>
-                    <a className="tools__tool" href="https://www.typescriptlang.org/docs/" target="blank"></a>
-                    <a className="tools__tool" href="https://trello.com/pl" target="blank"></a>
-
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="blank"> </a>
+                    <a className="tools__tool" href="https://reactjs.org/" target="blank"> </a>
+                    <a className="tools__tool" href="https://nodejs.org/en/docs/" target="blank"> </a>
+                    <a className="tools__tool" href="https://babeljs.io/docs/en/" target="blank"> </a>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="blank"> </a>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank"> </a>
+                    <a className="tools__tool" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON" target="blank"> </a>
+                    <a className="tools__tool" href="https://gulpjs.com/" target="blank"> </a>
+                    <a className="tools__tool" href="https://webpack.js.org/" target="blank"> </a>
+                    <a className="tools__tool" href="https://sass-lang.com/documentation/" target="blank"> </a>
+                    <a className="tools__tool" href="https://docs.npmjs.com/" target="blank"> </a>
+                    <a className="tools__tool" href="https://firebase.google.com/docs" target="blank"> </a>
+                    <a className="tools__tool" href="https://www.atlassian.com/software/jira" target="blank"> </a>
+                    <a className="tools__tool" href="https://github.com/" target="blank"> </a>
+                    <a className="tools__tool" href="https://git-scm.com/doc" target="blank"> </a>
+                    <a className="tools__tool" href="https://trello.com/pl" target="blank"> </a>
                 </div>
             </section>
     
