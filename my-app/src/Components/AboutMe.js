4dfb1cc5dc0e2e80c8d6015443817ee635f9ab96
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-04-28 20:49:20 +0200

    refactor

--- my-app/src/Components/AboutMe.js
+++ my-app/src/Components/AboutMe.js
@@ -1,22 +1,30 @@
 import React from "react";
 
 const AboutMe = () => {
-
     return (
-        <section name="aboutme" className="aboutme">
+        <section className="aboutme" id="aboutme">
             <div className="aboutme__image"></div>
             <div className="aboutme__description">
-                <h1>About me</h1>
+                <h2>About Me</h2>
                 <div className="separator"></div>
-                <p className="aboutme__text">I'm a creative person who likes to take part in teamwork 
-                to build new websites. Currently, I'm working as a freelancer on several projects, from minor 
-                one-page sites to some larger modules from more extensive projects. 
-                Last time I started considering changing from freelancing to a stable and long-term project.</p>
-                <a  target="blank" className="btn" href="">
-                    Download my CV</a>
+                <p className="aboutme__text">
+                    I'm a creative person who likes to take part in teamwork to build new
+                    websites. Currently, I'm working as a freelancer on several projects,
+                    ranging from minor one-page sites to larger modules for more extensive
+                    projects. Recently, I've started considering transitioning from
+                    freelancing to a more stable, long-term position.
+                </p>
+                <a
+                    className="btn"
+                    href="https://example.com/my-cv.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Download my CV
+                </a>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default AboutMe;
