cf0b9f38d7340c47990bb1bc87a37f1337f89f87
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-05-12 10:53:39 +0200

    .

--- my-app/src/Components/AboutMe.js
+++ my-app/src/Components/AboutMe.js
@@ -8,8 +8,8 @@ const AboutMe = () => {
                 <h2>About Me</h2>
                 <div className="separator"></div>
                 <p className="aboutme__text">
-                    I am a creative person who likes to participate in teamwork and likes 
-                    to learn new things. I am currently working as a freelancer on several 
+                    I am a creative person who likes to participate in teamwork and acquire new skills. 
+                    I am currently working as a freelancer on several 
                     projects but I am considering a change from freelancing to a long-term project, 
                     and preferably permanent employment.
                     I am characterized by creativity, high personal culture, resistance to stress.
